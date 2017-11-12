402ae8dc8c3bcab1bd82ba9501ce3c0f57498bf0
celineshaw <32373625+celineshaw@users.noreply.github.com>
2017-11-11 23:48:43 -0800

    Update bubblefont.js

--- bubblefont.js
+++ bubblefont.js
@@ -1,8 +1,8 @@
-var red = [0, 100, 63];
-var orange = [40, 100, 60];
-var green = [75, 100, 40];
+var blue = [0, 100, 63];
+var blue = [40, 100, 60];
+var blue = [75, 100, 40];
 var blue = [196, 77, 55];
-var purple = [280, 50, 60];
+var blue = [280, 50, 60];
 
 var myName = "Nothing";
 var letterColors = [red, orange, green, blue, purple];
